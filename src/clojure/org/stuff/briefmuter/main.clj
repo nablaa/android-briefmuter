63eac1364c8a1e799a735ba6dc0aaf9ae0b82892
Miika-Petteri Matikainen <miikapetteri@gmail.com>
2014-04-05 14:23:33 +0300

    Add multiple mute times

--- src/clojure/org/stuff/briefmuter/main.clj
+++ src/clojure/org/stuff/briefmuter/main.clj
@@ -5,22 +5,38 @@
         [neko.threading :only [on-ui]]
         [neko.ui :only [make-ui]]))
 
-(defn test-trigger-mute [_]
-  (toast "Muting for 5 sec")
+(def ^:const INTERVAL_5_MIN (* 5 60 1000))
+(def ^:const TEXT_5_MIN "5 min")
+(def ^:const INTERVAL_15_MIN (* 15 60 1000))
+(def ^:const TEXT_15_MIN "15 min")
+(def ^:const INTERVAL_30_MIN (* 30 60 1000))
+(def ^:const TEXT_30_MIN "30 min")
+(def ^:const INTERVAL_1_HOUR (* 60 60 1000))
+(def ^:const TEXT_1_HOUR "1 hour")
+
+(defn trigger-mute-interval [interval text]
+  (toast (str "Muting for " text))
   (let [alarm-manager (get-service :alarm)
         audio-manager (get-service :audio)
         pi (construct-pending-intent [:activity "org.stuff.briefmuter.UNMUTER"])]
     (.setRingerMode audio-manager android.media.AudioManager/RINGER_MODE_VIBRATE)
     (.set alarm-manager
           android.app.AlarmManager/ELAPSED_REALTIME_WAKEUP
-          (+ 5000 (android.os.SystemClock/elapsedRealtime))
+          (+ interval (android.os.SystemClock/elapsedRealtime))
           pi)))
 
 (def main-layout [:linear-layout {:orientation :vertical
                                   :layout-width :fill
                                   :layout-height :fill}
-                  [:button {:text "Mute for 5 sec",
-                            :on-click test-trigger-mute}]])
+                  [:button {:text TEXT_5_MIN,
+                            :on-click (fn [_] (trigger-mute-interval INTERVAL_5_MIN TEXT_5_MIN))}]
+                  [:button {:text TEXT_15_MIN,
+                            :on-click (fn [_] (trigger-mute-interval INTERVAL_15_MIN TEXT_15_MIN))}]
+                  [:button {:text TEXT_30_MIN,
+                            :on-click (fn [_] (trigger-mute-interval INTERVAL_30_MIN TEXT_30_MIN))}]
+                  [:button {:text TEXT_1_HOUR,
+                            :on-click (fn [_] (trigger-mute-interval INTERVAL_1_HOUR TEXT_1_HOUR))}]
+                  ])
 
 (defactivity org.stuff.briefmuter.MainActivity
   :def a

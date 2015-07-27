59ea6e36d846923679f521c01afeebece6427bac
Miika-Petteri Matikainen <miikapetteri@gmail.com>
2015-07-27 17:40:44 +0300

    Rename variable names

--- src/clojure/org/stuff/briefmuter/main.clj
+++ src/clojure/org/stuff/briefmuter/main.clj
@@ -13,15 +13,15 @@
 ;; access to all application resources.
 (res/import-all)
 
-(def ^:const INTERVAL_5_MIN (* 5 60 1000))
+(def ^:const INTERVAL_5_MIN_IN_MS (* 5 60 1000))
 (def ^:const TEXT_5_MIN "5 min")
-(def ^:const INTERVAL_15_MIN (* 15 60 1000))
+(def ^:const INTERVAL_15_MIN_IN_MS (* 15 60 1000))
 (def ^:const TEXT_15_MIN "15 min")
-(def ^:const INTERVAL_30_MIN (* 30 60 1000))
+(def ^:const INTERVAL_30_MIN_IN_MS (* 30 60 1000))
 (def ^:const TEXT_30_MIN "30 min")
-(def ^:const INTERVAL_45_MIN (* 45 60 1000))
+(def ^:const INTERVAL_45_MIN_IN_MS (* 45 60 1000))
 (def ^:const TEXT_45_MIN "45 min")
-(def ^:const INTERVAL_1_HOUR (* 60 60 1000))
+(def ^:const INTERVAL_1_HOUR_IN_MS (* 60 60 1000))
 (def ^:const TEXT_1_HOUR "1 hour")
 
 (def pending-intent (atom nil))
@@ -36,9 +36,9 @@
     (reset! pending-intent nil)
     (cancel :muting-triggered)))
 
-(defn trigger-mute-interval [activity interval text]
+(defn trigger-mute-interval [activity interval_ms text]
   (cancel-pending-intent)
-  (log/d "Mute triggered " {:interval interval :text text})
+  (log/d "Mute triggered " {:interval interval_ms :text text})
   (toast (str "Muting for " text))
   (let [alarm-manager (get-service :alarm)
         audio-manager (get-service :audio)
@@ -48,10 +48,10 @@
     (.setRingerMode audio-manager android.media.AudioManager/RINGER_MODE_VIBRATE)
     (.set alarm-manager
           android.app.AlarmManager/ELAPSED_REALTIME_WAKEUP
-          (+ interval (android.os.SystemClock/elapsedRealtime))
+          (+ interval_ms (android.os.SystemClock/elapsedRealtime))
           pi)
     (reset! pending-intent pi)
-    (.add calendar java.util.Calendar/MILLISECOND interval)
+    (.add calendar java.util.Calendar/MILLISECOND interval_ms)
     (log/d "Unmuting will happen at" {:time (.toString calendar)})
     (let [unmute-time (.format date-format (.getTime calendar))
           unmute-time-str (str "Muted until " unmute-time)
@@ -72,15 +72,15 @@
    [:text-view {:text "Mute period:"
                 :text-size [25 :dp]}]
    [:button {:text TEXT_5_MIN,
-             :on-click (fn [_] (trigger-mute-interval activity INTERVAL_5_MIN TEXT_5_MIN))}]
+             :on-click (fn [_] (trigger-mute-interval activity INTERVAL_5_MIN_IN_MS TEXT_5_MIN))}]
    [:button {:text TEXT_15_MIN,
-             :on-click (fn [_] (trigger-mute-interval activity INTERVAL_15_MIN TEXT_15_MIN))}]
+             :on-click (fn [_] (trigger-mute-interval activity INTERVAL_15_MIN_IN_MS TEXT_15_MIN))}]
    [:button {:text TEXT_30_MIN,
-             :on-click (fn [_] (trigger-mute-interval activity INTERVAL_30_MIN TEXT_30_MIN))}]
+             :on-click (fn [_] (trigger-mute-interval activity INTERVAL_30_MIN_IN_MS TEXT_30_MIN))}]
    [:button {:text TEXT_45_MIN,
-             :on-click (fn [_] (trigger-mute-interval activity INTERVAL_45_MIN TEXT_45_MIN))}]
+             :on-click (fn [_] (trigger-mute-interval activity INTERVAL_45_MIN_IN_MS TEXT_45_MIN))}]
    [:button {:text TEXT_1_HOUR,
-             :on-click (fn [_] (trigger-mute-interval activity INTERVAL_1_HOUR TEXT_1_HOUR))}]])
+             :on-click (fn [_] (trigger-mute-interval activity INTERVAL_1_HOUR_IN_MS TEXT_1_HOUR))}]])
 
 (defactivity org.stuff.briefmuter.MainActivity
   :key :main
